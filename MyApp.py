1a94cbfa0406d8f3410b755bc1c8d41f9bc93690
Ahmed-alzarooni <95509292+Ahmed-alzarooni@users.noreply.github.com>
2025-03-30 19:36:38 -0400

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -72,7 +72,7 @@ if st.session_state.authenticated:
     st.sidebar.title("Customize Graph")
 
     # Load and prepare dataset
-    file_path = 'cleaned_dataset.xlsx'
+    file_path = 'Dataset/cleaned_dataset.xlsx'
     data_df = pd.read_excel(file_path)
     cleaned_data_df = data_df.dropna(subset=['Series Name'])
     countries = cleaned_data_df['Country Name'].unique()
